957635bf5dd118cf80f56ad106b51a513e930d62
Andy <azha405@aucklanduni.ac.nz>
2023-06-05 18:38:53 +1200

    Finished task 1 - implemented all methods

--- src/main/java/nz/ac/auckland/se281/datastructures/Graph.java
+++ src/main/java/nz/ac/auckland/se281/datastructures/Graph.java
@@ -1,5 +1,6 @@
 package nz.ac.auckland.se281.datastructures;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -20,8 +21,45 @@ public class Graph<T extends Comparable<T>> {
   }
 
   public Set<T> getRoots() {
-    // TODO: Task 1.
-    throw new UnsupportedOperationException();
+    Set<T> lowestVertices = new HashSet<>();
+
+    if (isEquivalence()) {
+      for (T vertex : verticies) {
+        Set<T> equivalenceClass = getEquivalenceClass(vertex);
+        T lowestVertex = findLowestVertex(equivalenceClass);
+        lowestVertices.add(lowestVertex);
+      }
+    } else {
+      // find the verticies with no incoming edges and at least one outgoing edge using getSource
+      // and getDestination
+      for (T vertex : verticies) {
+        boolean hasIncomingEdge = false;
+        boolean hasOutgoingEdge = false;
+        for (Edge<T> edge : edges) {
+          if (edge.getDestination().equals(vertex)) {
+            hasIncomingEdge = true;
+          }
+          if (edge.getSource().equals(vertex)) {
+            hasOutgoingEdge = true;
+          }
+        }
+        if (!hasIncomingEdge && hasOutgoingEdge) {
+          lowestVertices.add(vertex);
+        }
+      }
+    }
+
+    return lowestVertices;
+  }
+
+  private T findLowestVertex(Set<T> vertices) {
+    T lowestVertex = null;
+    for (T vertex : vertices) {
+      if (lowestVertex == null || vertex.compareTo(lowestVertex) < 0) {
+        lowestVertex = vertex;
+      }
+    }
+    return lowestVertex;
   }
 
   public boolean isReflexive() {
@@ -42,28 +80,82 @@ public class Graph<T extends Comparable<T>> {
   }
 
   public boolean isSymmetric() {
-    // TODO: Task 1.
-    throw new UnsupportedOperationException();
+    int symmetricCount = 0;
+
+    // checks if the graph is symmetric
+    for (Edge<T> edge : edges) {
+      for (Edge<T> edge2 : edges) {
+        if (edge.getSource().equals(edge2.getDestination())
+            && edge.getDestination().equals(edge2.getSource())) {
+          symmetricCount++;
+        }
+      }
+    }
+    if (symmetricCount == edges.size()) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   public boolean isTransitive() {
-    // TODO: Task 1.
-    throw new UnsupportedOperationException();
+    for (Edge<T> edge1 : edges) {
+      for (Edge<T> edge2 : edges) {
+        if (edge1.getDestination().equals(edge2.getSource())) {
+          boolean transitiveExists = false;
+          for (Edge<T> edge3 : edges) {
+            if (edge1.getSource().equals(edge3.getSource())
+                && edge2.getDestination().equals(edge3.getDestination())) {
+              transitiveExists = true;
+              break;
+            }
+          }
+          if (!transitiveExists) {
+            return false; // Transitive relationship does not exist
+          }
+        }
+      }
+    }
+
+    return true; // All pairs satisfy the transitive relationship
   }
 
   public boolean isAntiSymmetric() {
-    // TODO: Task 1.
-    throw new UnsupportedOperationException();
+    for (Edge<T> edge1 : edges) {
+      for (Edge<T> edge2 : edges) {
+        if (edge1.getSource().equals(edge2.getDestination())
+            && edge1.getDestination().equals(edge2.getSource())) {
+          if (!edge1.getSource().equals(edge1.getDestination())) {
+            return false; // Anti-symmetric relationship does not exist
+          }
+        }
+      }
+    }
+    return true; // All pairs satisfy the anti-symmetric relationship
   }
 
   public boolean isEquivalence() {
-    // TODO: Task 1.
-    throw new UnsupportedOperationException();
+    // check for equivalence
+    if (isReflexive() && isSymmetric() && isTransitive()) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   public Set<T> getEquivalenceClass(T vertex) {
-    // TODO: Task 1.
-    throw new UnsupportedOperationException();
+    // get equivalence class
+    Set<T> equivalenceClass = new HashSet<T>();
+    if (isEquivalence()) {
+      for (Edge<T> edge : edges) {
+        if (edge.getSource().equals(vertex)) {
+          equivalenceClass.add(edge.getDestination());
+        }
+      }
+      return equivalenceClass;
+    } else {
+      return equivalenceClass;
+    }
   }
 
   public List<T> iterativeBreadthFirstSearch() {
